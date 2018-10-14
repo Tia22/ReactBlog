a51bfe96cfe2086a31008ed50f72d2cfbd2352d2
Tania Asimionesei <taniaasimionesei@Tanias-MacBook-Pro.local>
2018-10-14 21:06:33 +0400

    updating webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,41 @@
 module.exports = {
-  entry: ['./src/index.js'],
-  output: {
-    path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [
-      {
-        exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-          presets: ['react', 'es2015', 'stage-1']
-        }
+    entry: ['./src/index.js'],
+    output: {
+        path: __dirname,
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env',
+                        '@babel/react',{
+                            'plugins': ['@babel/plugin-proposal-class-properties']}]
+                }
 
-      }
-        ,
-        {
-            test: /\.css$/,
-            loader: 'style-loader!css-loader'
+            }
+            ,
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.css'],
+        modules: [
+            'node_modules'
+        ]
+    },
+    devServer: {
+        historyApiFallback: true,
+        contentBase: './',
+        watchOptions: {
+            aggregateTimeout: 300,
+            poll: 1000
         }
-    ]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx', '.css'],
-      modulesDirectories: [
-          'node_modules'
-      ]
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './',
-    watchOptions: {
-      aggregateTimeout: 300,
-      poll: 1000
     }
-  }
 };
